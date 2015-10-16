7fb6fe6beb59a220bf2c54260dc04fed58ea8680
hwctchen@foxmail.com <Administrator@PC-20151009CCEJ>
2015-10-16 11:59:14 +0800

    更改mainactivity

--- Demo1/src/com/example/demo1/MainActivity.java
+++ Demo1/src/com/example/demo1/MainActivity.java
@@ -18,6 +18,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class MainActivity extends FragmentActivity implements OnClickListener{
 
@@ -41,6 +42,8 @@ public class MainActivity extends FragmentActivity implements OnClickListener{
 			initFragments();
 			baseTabFrgamentAdapter = new BaseTabFrgamentAdapter(getSupportFragmentManager(), fragmentsList);
 			viewPager.setAdapter(baseTabFrgamentAdapter);
+			
+			Toast.makeText(getBaseContext(), "sssss", 0).show();
 			
 	}
 
